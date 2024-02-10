9d8824ca89e9e87448fde000baf9ff8fad61e2b2
Fernando Comunello <66500627+fercomunello@users.noreply.github.com>
2024-02-10 03:55:43 -0300

    Tooling for query execution plan analysis

--- postgresql/bank-statements.sql
+++ postgresql/bank-statements.sql
@@ -27,7 +27,8 @@ BEGIN
 END $$;
 SELECT * FROM customer_transactions;
 
--- [Summary] - Bank Statements
+-- [Summary] - Bank Statements => Plans analysis tool: tatiyants.com/pev/#/plans/new
+-- * Largest node (rows): 11 rows | Costliest node: 41,190.41
 EXPLAIN (ANALYZE, COSTS, VERBOSE, FORMAT JSON, BUFFERS)
 WITH account_totals AS (
     SELECT c.balance,
@@ -54,3 +55,24 @@ UNION ALL
  WHERE (tx.account_id = :acc_id) -- BITMAP INDEX SCAN
  ORDER BY tx.issued_at DESC -- INDEX SCAN
  LIMIT 10);
+
+-- Enables query execution metrics
+CREATE EXTENSION pg_stat_statements;
+
+-- Fetch recent metrics
+SELECT
+    query,
+    total_exec_time,
+    rows,
+    calls,
+    shared_blks_read,
+    shared_blks_hit,
+    blk_read_time,
+    blk_write_time,
+    shared_blks_written
+FROM pg_stat_statements
+ORDER BY total_exec_time DESC
+LIMIT 500;
+
+-- Reset metrics
+SELECT pg_stat_statements_reset();
