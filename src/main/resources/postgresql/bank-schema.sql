0c554ae9fdbc7ec076ac91ed583d3f0986e450c2
Fernando Comunello <66500627+fercomunello@users.noreply.github.com>
2024-02-15 01:24:28 -0300

    Update bank-schema.sql

--- src/main/resources/postgresql/bank-schema.sql
+++ src/main/resources/postgresql/bank-schema.sql
@@ -17,7 +17,7 @@ CREATE TYPE TXTYPE AS ENUM ('c', 'd');
 CREATE TABLE bank_accounts (
     id                BIGSERIAL,
     credit_limit      INT NOT NULL DEFAULT 0 CHECK ( credit_limit >= 0 ),
-    balance           INT NOT NULL DEFAULT 0,
+    balance           BIGINT NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 
@@ -26,7 +26,7 @@ CREATE TABLE bank_transactions (
     account_id      BIGINT NOT NULL,
     type            TXTYPE NOT NULL,
     amount          INT NOT NULL CHECK ( amount > 0 ),
-    description     VARCHAR(10),
+    description     VARCHAR(10) CHECK ( description IS NULL OR length(description) <= 10 ),
     issued_at       TIMESTAMPTZ NOT NULL DEFAULT now(),
     PRIMARY KEY (id),
     FOREIGN KEY (account_id) REFERENCES bank_accounts (id)
@@ -38,13 +38,13 @@ CREATE INDEX idx_tx_issued_filter ON bank_transactions (issued_at DESC);
 CREATE OR REPLACE FUNCTION process_bank_transaction(
     p_account_id BIGINT,
     p_type TXTYPE,
-    p_amount INT DEFAULT 0,
+    p_amount BIGINT DEFAULT 0,
     p_description VARCHAR(10) DEFAULT NULL
 )
-RETURNS TABLE (o_credit_limit INT, o_balance INT) AS $$
+RETURNS TABLE (o_credit_limit INT, o_balance BIGINT) AS $$
 DECLARE
     v_credit_limit INT NOT NULL DEFAULT 0;
-    v_balance      INT NOT NULL DEFAULT 0;
+    v_balance      BIGINT NOT NULL DEFAULT 0;
     v_lock_account BOOLEAN DEFAULT FALSE;
 BEGIN
     SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
@@ -75,7 +75,7 @@ BEGIN
         IF (FOUND AND p_type = 'd'::TXTYPE) THEN
             IF ((v_balance - p_amount) < -v_credit_limit) THEN
                 RAISE NOTICE 'Debit transaction rejected,
-                 insufficient credit limit for this account.';
+                 insufficient credit credit limit for this account.';
                 ROLLBACK;
             END IF;
 
@@ -105,8 +105,8 @@ REVOKE DELETE ON bank_transactions FROM duke;
 
 INSERT INTO bank_accounts
        (id, credit_limit, balance)
-VALUES (1, 1000 * 100, 0), -- R$1,000.00
-       (2, 800 * 100, 0), -- R$8,000.00
-       (3, 10000 * 100, 0), -- R$10,000.00
-       (4, 100000 * 100, 0), -- R$100,000.00
-       (5, 5000 * 100, 0); -- R$5,000.00
+VALUES (1, 1000 * 100, 0), -- $1,000.00 == 100000
+       (2, 80000 * 100, 0), -- $80,000.00 == 80000
+       (3, 10000 * 100, 0), -- $10,000.00 == 1000000
+       (4, 100000 * 100, 0), -- $100,000.00 == 10000000
+       (5, 5000 * 100, 0); -- $5,000.00 == 500000
